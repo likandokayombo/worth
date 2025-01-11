272ed6bb7a8b57752e4a57392b407dcfe7b3e7c4
Likando Kayomboe <likandokayombo2030@gmail.com>
2025-01-11 12:03:17 +0200

    Preloader Wrapper

--- els.js
+++ els.js
@@ -18,3 +18,14 @@ body.addEventListener("click", (e) => {
     nav.classList.remove("active");
   }
 });
+
+
+// Wait for the window to load
+window.onload = function() {
+  // Set a timeout to simulate loading delay
+  setTimeout(function() {
+      // Hide preloader and show content
+      document.getElementById('preloader').style.display = 'none';
+      document.getElementById('content').style.display = 'block';
+  }, 2000); // Adjust this time to simulate longer or shorter loading
+};
